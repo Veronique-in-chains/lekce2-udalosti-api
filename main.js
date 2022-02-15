2766ad07f1b2c305a0ebc0f2dfab379a8b4734de
Veronique.in.chains <veronikabosakova@gmail.com>
2022-02-15 18:51:57 +0100

    using the classList and add and remove class

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // tady je místo pro náš program
-console.log("jsem nalinkován")
+
+let nadpis = document.querySelector("h1");
+
+nadpis.classList.add("zluty");
+nadpis.classList.remove("zluty");
+
+let ctverecek = document.querySelector("div.zluty");
+ctverecek.classList.add("ctverecek");
